fa922e175a36591cdfe71708fd5e6a844879c3e7
Colleen O'Rourke <colleen@sentry.io>
2020-06-12 10:45:19 -0700

    change picture

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm a really cute cat.</p>
+        <p>I'm a reeeeally cute cat.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
