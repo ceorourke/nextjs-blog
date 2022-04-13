9a4f5cd4f078c7598bca35ce985bff7eec4e8d27
Colleen O'Rourke <colleen@sentry.io>
2022-04-13 10:32:47 -0700

    Test

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/global.css'
 import * as Sentry from '@sentry/node'
 
+console.log("this is a test")
 Sentry.init({ 
 	dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 	release: process.env.VERCEL_GITHUB_COMMIT_SHA,
